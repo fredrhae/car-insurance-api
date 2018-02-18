10cbdd5e9ba3d94e8384f38170936c7f540c901a
Fred Rhae <fredrhae@gmail.com>
2018-02-18 20:38:21 -0300

    Added the calculation and tests for modifier table

--- src/test/java/car/insurance/company/carinsuranceapi/rest/RestApiControllerV1Test.java
+++ src/test/java/car/insurance/company/carinsuranceapi/rest/RestApiControllerV1Test.java
@@ -2,9 +2,14 @@ package car.insurance.company.carinsuranceapi.rest;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestApiControllerV1.class)
 public class RestApiControllerV1Test {
 
     @Before
